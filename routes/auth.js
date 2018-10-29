e2a6008670e70bf4590b120a31e1d96ded5d3962
fakorede <abiolafakorede@gmail.com>
2018-10-29 16:04:22 +0100

    auth callback and remote app setup

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,10 @@ const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+    // Successful authentication
+    res.redirect('/dashboard');
+});
+
 module.exports = router;
