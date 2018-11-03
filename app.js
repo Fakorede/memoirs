8c9f297c7ac3ef5277a041f7b83f9f5ac165996b
fakorede <abiolafakorede@gmail.com>
2018-11-03 11:29:30 +0100

    method override installed to handle form updates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -47,6 +48,9 @@ const app = express();
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Method Override middleware
+app.use(methodOverride('_method'));
 
 // Handlebars middleware
 app.engine('handlebars', exphbs({
