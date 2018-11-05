9cea9e2a24e3fe633d596cef13b05e13e50ca47c
fakorede <abiolafakorede@gmail.com>
2018-11-05 02:14:30 +0100

    edit access control and helper created for edit icon

--- routes/stories.js
+++ routes/stories.js
@@ -42,9 +42,13 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
         _id: req.params.id
     })
     .then(story => {
-        res.render('stories/edit', { 
-            story: story
-        });
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', { 
+                story: story
+            });
+        }
     });
 });
 
