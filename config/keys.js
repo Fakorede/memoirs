4a5d2daefff81695310fea2e26a57b67c22bfc4c
fakorede <abiolafakorede@gmail.com>
2018-11-10 23:51:36 +0100

    prep production key variables for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://fakorede:fakorede12@ds145083.mlab.com:45083/memoirs',
-    googleClientID: '463619078994-th5m49si0p4kaefee0pnoqcgaeo96m7g.apps.googleusercontent.com',
-    googleClientSecret: 'GMMgSETc04sC_aBkry5L0WLx'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');   
+} else {
+    module.exports = require('./keys_dev');   
 }
