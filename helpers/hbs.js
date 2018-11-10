94144a4eb567b3ef6e9780e4f81023cc7c6c200b
fakorede <abiolafakorede@gmail.com>
2018-11-10 23:19:39 +0100

    access control on edit icon

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -25,7 +25,7 @@ module.exports = {
             if(floating) {
                 return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red"><i class="fa fa-pencil"></i></a>`;
             } else {
-                return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red"><i class="fa fa-pencil"></i></a>`;
+                return `<a href="/stories/edit/${storyId}"><i class="fa fa-pencil"></i></a>`;
             }
         } else {
             return '';
